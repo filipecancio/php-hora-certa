a9f0bd86536e57a83c04fd10f109cab15f0dfd67
Cancio <filipe.cancio@outlook.com>
2020-11-26 09:04:11 -0300

    parte sem lógica

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Hora Certa</title>
+</head>
+<body>
+    <div class="hr-container">
+        <img class="hr-img" src="./assets/img/moon.png"/>
+        <div class="hr-message">
+            <p>Agora são</p>
+            <p> 22:00h</p>
+            <p>. Boa noite!</p>
+        </div>
+    </div>
+</body>
+</html>
